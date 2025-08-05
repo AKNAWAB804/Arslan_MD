dde241b0c268336e707b98a77d254083e15fbb85
AKNAWAB804 <aknawab804@gmail.com>
2025-08-05 12:54:55 +0500

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "",
+SESSION_ID: process.env.SESSION_ID || "ARSLAN-MD~eyJub2lzZUtleSI6eyJwcml2YXRlIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiY0RTd1N0d3lXRSsxZURDVWFtSExBdVBDTGZwcVVxTzRYRUx4ekplcjgwTT0ifSwicHVibGljIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiSnR1OUNiV25CZTlFdGpKNGhtN3duQUVPREQ2ZkJQcWxjY1k3Z0l3ZnNBZz0ifX0sInBhaXJpbmdFcGhlbWVyYWxLZXlQYWlyIjp7InByaXZhdGUiOnsidHlwZSI6IkJ1ZmZlciIsImRhdGEiOiJDQSt6Rm0waU85U1FUTytjTStxcXNCa0tqcUp4T0pSN2htZkExMmVQV1dVPSJ9LCJwdWJsaWMiOnsidHlwZSI6IkJ1ZmZlciIsImRhdGEiOiJ4ZW5XLzhHa1JyS1NzWWdLdnBpSVdXQ3U0WnVLUDZqUmx1TkpVeTZsQm44PSJ9fSwic2lnbmVkSWRlbnRpdHlLZXkiOnsicHJpdmF0ZSI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6IkVFakNJazVoaW56czRQZWk5eFFJTFUxU21aZzRsYS8wKzlsNHZYWkNTVUk9In0sInB1YmxpYyI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6IlpHOWNGQkVrb0szNFA3NVFGZ0kveEtWVEFzOG1meUg0SHIvektHNnk3aXc9In19LCJzaWduZWRQcmVLZXkiOnsia2V5UGFpciI6eyJwcml2YXRlIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiQVBqQmFuMUNiYmNTcGlLSHdZMGFRNGNyV1RPdXVUNlc0ZGo5NWY4S1lrVT0ifSwicHVibGljIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiS0VzVjNUTUp5Smd2Wk12eDc2Wjd6MXJ1Vyt4UUIxS3FqUVNWRVp6UHQzdz0ifX0sInNpZ25hdHVyZSI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6IkdYYzNWMGVjQ0MwMXBOcjd3TkNjTjQxdXVvbWRJSUdsNWZaMFZWSE5taFNyb1JMUUUzUDlVUnZBZzgxSlFPOHhVOGJWaWR6UlowTDlVM1Z4UTREQmd3PT0ifSwia2V5SWQiOjF9LCJyZWdpc3RyYXRpb25JZCI6MzQsImFkdlNlY3JldEtleSI6ImNCNm51SzlTQVpIK2k3Nzl3YnVJY3dQR1ZBeEY1TktzdXVPMXA1VXU5Sk09IiwicHJvY2Vzc2VkSGlzdG9yeU1lc3NhZ2VzIjpbXSwibmV4dFByZUtleUlkIjozMSwiZmlyc3RVbnVwbG9hZGVkUHJlS2V5SWQiOjMxLCJhY2NvdW50U3luY0NvdW50ZXIiOjAsImFjY291bnRTZXR0aW5ncyI6eyJ1bmFyY2hpdmVDaGF0cyI6ZmFsc2V9LCJyZWdpc3RlcmVkIjp0cnVlLCJwYWlyaW5nQ29kZSI6IjU0TVJYOVc3IiwibWUiOnsiaWQiOiI5MjMxNDc2ODI4MjI6MjhAcy53aGF0c2FwcC5uZXQiLCJsaWQiOiIzNTA5OTAwOTY1OTA3OToyOEBsaWQifSwiYWNjb3VudCI6eyJkZXRhaWxzIjoiQ01qcnI2OEZFSlB2eHNRR0dBRWdBQ2dBIiwiYWNjb3VudFNpZ25hdHVyZUtleSI6ImRNRzFPemg3YWlOZkZLL0swWnJBVDdUcUxuamNMS0IvWDhPM21ISmRwRlk9IiwiYWNjb3VudFNpZ25hdHVyZSI6ImVySWhVeUZocEJjc0kwbnBSWU9pcFpoMkJBUm9Za2luV0VEbkRtMTdQczBpS25ndEl0S3VHbG1wbW8rKzlSZzF0YnBFalJIb2ZuelQ3MlJmR0h3dkNnPT0iLCJkZXZpY2VTaWduYXR1cmUiOiIybU5PMURlYnB3K3pRUHBTc0VIWjR2cmg3L2hKTmVGYlJWZkNJT09jMHJWWWR2V0YvMWxRTlpRTkN0ZnZ5MHpWdUJpbnVlQW9XOHRqLzUyQnByd2NnUT09In0sInNpZ25hbElkZW50aXRpZXMiOlt7ImlkZW50aWZpZXIiOnsibmFtZSI6IjkyMzE0NzY4MjgyMjoyOEBzLndoYXRzYXBwLm5ldCIsImRldmljZUlkIjowfSwiaWRlbnRpZmllcktleSI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6IkJYVEJ0VHM0ZTJvalh4U3Z5dEdhd0UrMDZpNTQzQ3lnZjEvRHQ1aHlYYVJXIn19XSwicGxhdGZvcm0iOiJhbmRyb2lkIiwicm91dGluZ0luZm8iOnsidHlwZSI6IkJ1ZmZlciIsImRhdGEiOiJDQklJQWc9PSJ9LCJsYXN0QWNjb3VudFN5bmNUaW1lc3RhbXAiOjE3NTQzODAxODMsImxhc3RQcm9wSGFzaCI6IjJWNzdxVSJ9",
 // add your Session Id 
 AUTO_STATUS_SEEN: process.env.AUTO_STATUS_SEEN || "true",
 // make true or false status auto seen
@@ -13,7 +13,11 @@ AUTO_STATUS_REPLY: process.env.AUTO_STATUS_REPLY || "false",
 // make true if you want auto reply on status 
 AUTO_STATUS_REACT: process.env.AUTO_STATUS_REACT || "true",
 // make true if you want auto reply on status 
-AUTO_STATUS_MSG: process.env.AUTO_STATUS_MSG || "*SEEN YOUR STATUS BY Arslan_MD 🇵🇰*",
+AUTO_STATUS_MSG: process.env.AUTO_STATUS_MSG || "**❁بِسْــــــــــــــــــمِ اﷲِالرَّحْمَنِ اارَّحِيم❁۔*
+
+*_اَللّٰھُمَّ صَــّلِ عَلٰی ,مُحَمَّدٍ وَّعَلٰٓی اٰلِ مُحَمَّدٍ کَمَا صَلَّیْتَ عَلٰٓی اِبْرَاھِیْمَ وَعَلٰٓی اٰلِ اِبْرَاھِیْمَ اِنَّکَ حَمِیْدٌ مَّجِیْدٌ_*🫠
+
+ *_اَللّٰھُمَّ بَارِکْ عَلٰی مُحَمَّدٍ وَّعَلٰٓی اٰلِ مُحَمَّدٍ کَمَا بَارَکْتَ عَلٰٓی اِبْرَاھِیْمَ وَعَلٰٓی اٰلِ اِبْرَاھِیْمَ اِنَّکَ حَمِیْدٌ مَّجِیْدٌ،_*🫠🇵🇰*",
 // set the auto reply massage on status reply  
 ANTI_DELETE: process.env.ANTI_DELETE || "true",
 // set true false for anti delete     
@@ -35,7 +39,7 @@ BOT_NAME: process.env.BOT_NAME || "Arslan_MD",
 // add bot namw here for menu
 AUTO_STATUS_REACT: process.env.AUTO_STATUS_REACT || "true",
 // true to get auto status react
-STICKER_NAME: process.env.STICKER_NAME || "Arslan_MD",
+STICKER_NAME: process.env.STICKER_NAME || "AHMAD_MD",
 // type sticker pack name 
 CUSTOM_REACT: process.env.CUSTOM_REACT || "false",
 // make this true for custum emoji react    
@@ -45,7 +49,7 @@ DELETE_LINKS: process.env.DELETE_LINKS || "false",
 // automatic delete links witho remove member 
 OWNER_NUMBER: process.env.OWNER_NUMBER || "923237045919",
 // add your bot owner number
-OWNER_NAME: process.env.OWNER_NAME || "ArslanMD Official",
+OWNER_NAME: process.env.OWNER_NAME || "Ahmad804 Official",
 // add bot owner name
 DESCRIPTION: process.env.DESCRIPTION || "*©𝙿𝙾𝚆𝙴𝚁𝙴𝙳 𝙱𝚈 Arslan_MD*",
 // add bot owner name    
@@ -55,13 +59,12 @@ LIVE_MSG: process.env.LIVE_MSG || "> I'm alive*Arslan_MD*🇵🇰",
 // add alive msg here 
 READ_MESSAGE: process.env.READ_MESSAGE || "false",
 // Turn true or false for automatic read msgs
-AUTO_REACT: process.env.AUTO_REACT || "false",
+AUTO_REACT: process.env.AUTO_REACT || "true",
 // make this true or false for auto react on all msgs
-ANTI_BAD: process.env.ANTI_BAD || "false",
+ANTI_BAD: process.env.ANTI_BAD || "trur",
 // false or true for anti bad words  
-MODE: process.env.MODE || "public",
-// make bot public-private-inbox-group 
-ANTI_LINK_KICK: process.env.ANTI_LINK_KICK || "false",
+MODE: process.env.MODE || "private // make bot public-private-inbox-group 
+ANTI_LINK_KICK: process.env.ANTI_LINK_KICK || "true",
 // make anti link true,false for groups 
 AUTO_STICKER: process.env.AUTO_STICKER || "false",
 // make true for automatic stickers 
